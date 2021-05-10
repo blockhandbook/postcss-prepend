6b834329cdd3bb5b4e45551bc8ea79d773ed1be6
Lee Shadle <leeshadle@gmail.com>
2021-05-10 13:15:51 -0700

    updating plugin to work with postcss 8.0.0+

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-let postcss = require('postcss');
+module.exports = (opts = {}) => {
+	// checkOpts(opts)
+	return {
+	  postcssPlugin: 'postcss-prepend',
+	  Once (root, { result }) {
+		  console.log( root )
+			  root.walkAtRules(atrule => {
+				  console.log( atrule )
+				  atrule.selectors = atrule.selectors.map( function (selector) {
+					  if(/^([0-9]*[.])?[0-9]+\%$|^from$|^to$/.test(selector)) {
+							  // This is part of a keyframe
+							  return selector;
+					  }
 
-module.exports = postcss.plugin('postcss-prepend-selector', function (opts) {
-    opts = opts || {};
-    return function (css) {
-        css.walkRules(function (rule) {
-            rule.selectors = rule.selectors.map( function (selector) {
-                if(/^([0-9]*[.])?[0-9]+\%$|^from$|^to$/.test(selector)) {
-                    // This is part of a keyframe
-                    return selector;
-                }
+					  if (selector.startsWith(opts.selector.trim())) {
+							  return selector;
+					  }
 
-                if (selector.startsWith(opts.selector.trim())) {
-                    return selector;
-                }
-
-                return opts.selector + selector;
-            });
-        });
-    };
-});
+					  return opts.selector + selector;
+			  });
+			  })
+	  }
+	}
+  }
+  module.exports.postcss = true
